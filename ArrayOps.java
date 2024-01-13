f9157234bd32c593d0dad964215aa1dba20160f8
rotembreslauer <rotem.breslauer@post.runi.ac.il>
2024-01-13 18:04:31 +0200

    HW04 2

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,7 +1,7 @@
 public class ArrayOps {
     public static void main(String[] args) {
 
-        int[] array = {2,8,3,7,8};
+        int[] array = {1,2,3,5,5};
 
        System.out.println(secondMaxValue(array));
        
@@ -29,21 +29,34 @@ public class ArrayOps {
         int n = array.length;
         int max1 = 0; 
         int max2 = 0; 
+        int count = 0;
 
         for(int i = 0; i < n; i ++){
             if (array[i] > max1){
                 max1 = array[i]; // max of the array
-            } 
-        }
-        for(int i = 0; i < n; i ++){
-            if((array[i] > max2) && (array[i] <= max1)){
-                max2 = array[i]; // second max of array
             }
         }
-        
-        return max2;
-    }
+        for(int i = 0; i < n; i++){
+            if(array[i] == max1){
+                count ++;
+            }
+         }
+         if (count > 1){
+            max2 = max1;
+         }
+         else{
+            for(int i = 0; i < n; i ++){
 
+                if(((array[i] > max2) && (array[i] < max1))){
+                    max2 = array[i]; // second max of array
+                }
+                }
+         }
+        
+            return max2;
+
+        } 
+       
 
     public static boolean inArray(int [] array, int num) {
 
